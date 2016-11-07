ed5a4678d39052e4ff31d167dc047222ad777794
YongWoo Jeon <yongwoo.jeon@navercorp.com>
2016-11-07 17:06:28 +0900

    apply reviews

--- worker.js
+++ worker.js
@@ -1,10 +1,26 @@
-// import util script
-self.importScripts("import.js");
+function grayScale(image) {
+	var masterLength = image.data.length || 0;
+	var grey = 0;
+
+	for (var i = 0; i < masterLength; i += 4){
+		grey = (image.data[i] +	// R channel
+			   image.data[i + 1] + // G channel
+			   image.data[i + 2])/3; // B channel
+
+		image.data[i] = grey;
+		image.data[i + 1] = grey;
+		image.data[i + 2] = grey;
+		image.data[i + 3] = 255;
+	}
+
+	return image;
+}
+
 
 // receive message
 self.onmessage = function (event) {
 	// manipulation data
-	var greyImage = luminosityGrey(event.data);
+	var greyImage = grayScale(event.data);
 	// send data
 	self.postMessage(greyImage);
 };
