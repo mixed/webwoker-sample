39ce3eb5926eb18a37eea46cea8a9a98d0a76761
YongWoo Jeon <yongwoo.jeon@navercorp.com>
2016-11-07 23:15:17 +0900

    apply reviews2

--- worker.js
+++ worker.js
@@ -1,26 +1,26 @@
 function grayScale(image) {
-	var masterLength = image.data.length || 0;
-	var grey = 0;
+    var masterLength = image.data.length || 0;
+    var grey = 0;
 
-	for (var i = 0; i < masterLength; i += 4){
-		grey = (image.data[i] +	// R channel
-			   image.data[i + 1] + // G channel
-			   image.data[i + 2])/3; // B channel
+    for (var i = 0; i < masterLength; i += 4) {
+        grey = (image.data[i] + // R channel
+            image.data[i + 1] + // G channel
+            image.data[i + 2]) / 3; // B channel
 
-		image.data[i] = grey;
-		image.data[i + 1] = grey;
-		image.data[i + 2] = grey;
-		image.data[i + 3] = 255;
-	}
+        image.data[i] = grey;
+        image.data[i + 1] = grey;
+        image.data[i + 2] = grey;
+        image.data[i + 3] = 255;
+    }
 
-	return image;
+    return image;
 }
 
 
-// receive message
-self.onmessage = function (event) {
-	// manipulation data
-	var greyImage = grayScale(event.data);
-	// send data
-	self.postMessage(greyImage);
+// received message
+self.onmessage = function(event) {
+    // manipulation data
+    var greyImage = grayScale(event.data);
+    // send data
+    self.postMessage(greyImage);
 };
